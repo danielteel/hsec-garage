9b7c2b973b66bbe06dfac2f6d5cdf46d4fd4578d
Dan Teel <danjteel@gmail.com>
2024-04-20 21:42:02 -0600

    testing uploading image to express server

--- src/camera.cpp
+++ src/camera.cpp
@@ -5,32 +5,32 @@
 camera_config_t cameraConfig;
 
 void initConfig(camera_config_t& c){
-  c.ledc_channel = LEDC_CHANNEL_0;
-  c.ledc_timer = LEDC_TIMER_0;
-  c.pin_d0 = Y2_GPIO_NUM;
-  c.pin_d1 = Y3_GPIO_NUM;
-  c.pin_d2 = Y4_GPIO_NUM;
-  c.pin_d3 = Y5_GPIO_NUM;
-  c.pin_d4 = Y6_GPIO_NUM;
-  c.pin_d5 = Y7_GPIO_NUM;
-  c.pin_d6 = Y8_GPIO_NUM;
-  c.pin_d7 = Y9_GPIO_NUM;
-  c.pin_xclk = XCLK_GPIO_NUM;
-  c.pin_pclk = PCLK_GPIO_NUM;
-  c.pin_vsync = VSYNC_GPIO_NUM;
-  c.pin_href = HREF_GPIO_NUM;
-  c.pin_sscb_sda = SIOD_GPIO_NUM;
-  c.pin_sscb_scl = SIOC_GPIO_NUM;
-  c.pin_pwdn = PWDN_GPIO_NUM;
-  c.pin_reset = RESET_GPIO_NUM;
-  c.xclk_freq_hz = 20000000;
-  c.frame_size = FRAMESIZE_QVGA;
-  c.pixel_format = PIXFORMAT_JPEG; // for streaming
-  //c.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
-  c.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
-  c.fb_location = CAMERA_FB_IN_PSRAM;
-  c.jpeg_quality = 12;
-  c.fb_count = 1;
+    c.ledc_channel = LEDC_CHANNEL_0;
+    c.ledc_timer = LEDC_TIMER_0;
+    c.pin_d0 = Y2_GPIO_NUM;
+    c.pin_d1 = Y3_GPIO_NUM;
+    c.pin_d2 = Y4_GPIO_NUM;
+    c.pin_d3 = Y5_GPIO_NUM;
+    c.pin_d4 = Y6_GPIO_NUM;
+    c.pin_d5 = Y7_GPIO_NUM;
+    c.pin_d6 = Y8_GPIO_NUM;
+    c.pin_d7 = Y9_GPIO_NUM;
+    c.pin_xclk = XCLK_GPIO_NUM;
+    c.pin_pclk = PCLK_GPIO_NUM;
+    c.pin_vsync = VSYNC_GPIO_NUM;
+    c.pin_href = HREF_GPIO_NUM;
+    c.pin_sscb_sda = SIOD_GPIO_NUM;
+    c.pin_sscb_scl = SIOC_GPIO_NUM;
+    c.pin_pwdn = PWDN_GPIO_NUM;
+    c.pin_reset = RESET_GPIO_NUM;
+    c.xclk_freq_hz = 20000000;
+    c.frame_size = FRAMESIZE_VGA;//FRAMESIZE_HD;
+    c.pixel_format = PIXFORMAT_JPEG; // for streaming
+    //c.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+    c.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+    c.fb_location = CAMERA_FB_IN_PSRAM;
+    c.jpeg_quality = 10;
+    c.fb_count = 1;
 }
 
 void cameraSetup(){
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <Arduino.h>
 #include <WiFiManager.h> 
+#include <WiFi.h>
+#include <Esp.h>
 #include "camera.h"
 WiFiManager wifiManager;
-
+WiFiClient Client;
 
 
 void setup() {
+  Serial.begin(115200);
+  Serial.println("Initializing...");
+
   WiFiManagerParameter deviceNameParameter("name", "Device Name", "garage", 31);
   wifiManager.addParameter(&deviceNameParameter);
 
@@ -14,17 +19,36 @@ void setup() {
 
   wifiManager.setConfigPortalTimeout(60);
   wifiManager.setConnectTimeout(60);
-  wifiManager.autoConnect("DnD", "powerboner69");
+  wifiManager.autoConnect("HSEC Dev Setup", "powerboner69");
 
+  Serial.println("Auto connect returned...");
   cameraSetup();
+  Serial.println("camera setup...");
 }
 
 void loop() {
-    CAMERA_CAPTURE capture;
-    if (cameraCapture(capture)){
-        //do stuff with capture.jpgBuff / jpgBuffLen
-        cameraCaptureCleanup(capture);
+  
+    if (Client.connect("192.168.50.178", 3000)){
+      CAMERA_CAPTURE capture;
+      if (cameraCapture(capture)){
+          //do stuff with capture.jpgBuff / jpgBuffLen
+          Client.println("POST / HTTP/1.0");
+          Client.println("Host: 192.168.50.178:3000");
+          Client.println("Content-Length: "+String(capture.jpgBuffLen));
+          Client.println("Content-Type: image/jpeg");
+          Client.println("Connection: close");
+          Client.println();
+          Client.write(capture.jpgBuff, capture.jpgBuffLen);
+          cameraCaptureCleanup(capture);
+          Serial.print("captured ");
+      }else{
+        Serial.print("failed to capture ");
+      }
+      
+      Serial.print(ESP.getFreeHeap());
+      Serial.print(" ");
+      Serial.println(ESP.getFreePsram());
+      Client.stop();
     }
-    
     delay(2000);
 }
